94dbea7d7c275e595b6db6f1f03e099664c206da
nurali-code <nur3.dav.97@gmail.com>
2024-05-09 01:36:47 +0500

    new pages

--- js/scripts.js
+++ js/scripts.js
@@ -15,20 +15,29 @@ $('.tab__btn').on('click', function (e) {
     }
 });
 
+$('.position__btn').on('click', function (e) {
+    $('.position__btn').removeClass('is__active');
+    $(this).addClass('is__active');
+    const trId = $(this).attr('data-possbtn');
+    $('.position-title').css('display', 'none');
+    $('[data-position=' + trId + ']').css('display', 'flex');
+});
+
 function hideModals() {
-    $('.modal').removeClass('is__active').fadeOut(200);
+    $('.modal').removeClass('is__active').fadeOut(300);
     $('body, .modal').removeClass('is__active');
 };
 
 $(function () {
     function showModal(id) {
-        $(id).fadeIn(100);
+        $(id).fadeIn(300);
         console.log(id);
         $('body').addClass('is__active');
     }
 
-    $('.modal-close').on('click', () => { hideModals();});
-    $('[data-id]').on('click', () => { showModal('#modalWallet'); });
+    $('.modal-close').on('click', () => { hideModals(); });
+
+    $('[data-id], a.css-1fn1rmh').on('click', () => { showModal('#modalWallet'); });
 
     $('[data-modal]').on('click', function (e) {
         e.preventDefault();
@@ -40,8 +49,10 @@ $(function () {
             ($(e.target).parents('.modal-content').length) ||
             ($(e.target).parents('.nav__item').length) ||
             ($(e.target).parents('.css-fjcmtc').length) ||
+            ($(e.target).parents('.css-1fn1rmh').length) ||
             ($(e.target).hasClass('btn')) ||
             ($(e.target).hasClass('css-fjcmtc')) ||
+            ($(e.target).hasClass('css-1fn1rmh')) ||
             ($(e.target).hasClass('modal-content'))
         )) { hideModals(); }
     });
